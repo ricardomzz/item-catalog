9600cf489c2e50dcf185350839e95f809eb6dfc5
Ricardo Mazzini <ricardomzz@gmail.com>
2017-04-03 17:49:12 -0400

    Implemented: -require_login decorator -gdisconnect route for logging out -template displays login/logout links depending on whether session exists

--- project.py
+++ project.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template, request, redirect, jsonify, url_for
+from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import IntegrityError
 from database_setup import Base, Category, Item
 from flask import session as login_session
+
+from apiclient import discovery
+import httplib2
+from oauth2client import client
+
+import json
+from functools import wraps
+
 
 
 app = Flask(__name__)
@@ -14,8 +22,56 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+def require_login(function):
+    @wraps(function)
+    def wrapper(*args,**kwargs):
+        if 'userid' not in login_session:
+            flash('login required')
+            return redirect('/')
+        else:
+            return function(*args,**kwargs)
+    return wrapper
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # (Receive auth_code by HTTPS POST)
+    auth_code=request.data
+
+    # If this request does not have `X-Requested-With` header, this could be a CSRF
+    if not request.headers.get('X-Requested-With'):
+        abort(403)
+
+    # Set path to the Web application client_secret_*.json file you downloaded from the
+    # Google API Console: https://console.developers.google.com/apis/credentials
+    CLIENT_SECRET_FILE = 'client_secret.json'
+
+    # Exchange auth code for access token, refresh token, and ID token
+    credentials = client.credentials_from_clientsecrets_and_code(
+        CLIENT_SECRET_FILE,
+        ['https://www.googleapis.com/auth/drive.appdata', 'profile', 'email'],
+        auth_code)
+
+    # Call Google API
+    http_auth = credentials.authorize(httplib2.Http())
+    #drive_service = discovery.build('drive', 'v3', http=http_auth)
+    #appfolder = drive_service.files().get(fileId='appfolder').execute()
+
+    # Get profile info from ID token
+    userid = credentials.id_token['sub']
+    email = credentials.id_token['email']
+    login_session['credentials'] = credentials.to_json()
+    login_session['userid'] = userid
+    flash('logged in as %s' %login_session['userid'])
+    return userid
+
+@app.route('/gdisconnect', methods=['GET'])
+def gdisconnect():
+    login_session.clear()
+    return redirect('/')
+
 # Create a new category
 @app.route('/category/new/', methods=['GET', 'POST'])
+@require_login
 def newCategory():
     if request.method == 'POST':
         newCategory = Category(name=request.form['name'])
@@ -34,7 +90,6 @@ def newCategory():
 @app.route('/category/')
 def showCategories():
     categories = session.query(Category).all()
-    # return "This page will show all my categories"
     return render_template('categories.html', categories=categories)
 
 #Edit a Category
@@ -99,5 +154,6 @@ def deleteItem(category_id,item_id):
 
 
 if __name__ == '__main__':
+    app.secret_key = 'Random_Secret_String'
     app.debug = True
     app.run(host='0.0.0.0', port=8080)
